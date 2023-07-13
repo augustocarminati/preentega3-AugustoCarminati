e7f1d54f34644eb1be19a3dae1486dec110e0393
Augusto Carminati <augusto_carminati04@hotmail.com>
2023-07-12 23:03:24 -0300

    se trabajo sobre el codigo js y html

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let carrito = [];
 
 // declaro una variable para obtener productos de la tienda por su id
-
 const contenidoTienda = document.getElementById("contenidoTienda");
+
+
+//declaro variable para ver los productos del carrito
+const mostrarCarrito = document.getElementById("mostrarCarrito");
+
+const carritoContenedor = document.getElementById("carritoContenedor");
 
 //declaro metodo para recorrer carrito y creo contenedor para productos con sus propiedades
 
@@ -10,9 +15,9 @@ productos.forEach((products) => {
     let contenido = document.createElement("div");
     contenidoTienda.className = "tarjeta-productos";
     contenido.innerHTML = `
-     <img src="${products.imagen}">
-     <h3>${products.nombre}</h3>
-     <p class="precio-producto">$ ${products.precio}</p>
+    <img src="${products.imagen}">
+    <h3>${products.nombre}</h3>
+    <p class="precio-producto">$ ${products.precio}</p>
     `;
 
     contenidoTienda.append(contenido);
@@ -22,5 +27,28 @@ productos.forEach((products) => {
     comprar.className = "boton-comprar"
 
     contenido.append(comprar)
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            imagen: products.imagen,
+            nombre: products.nombre,
+            precio: products.precio,
+        });
+        console.log(carrito);
+    })
 });
 
+mostrarCarrito.addEventListener("click", () => {
+    const encabezado = document.createElement("div");
+    encabezado.className = "encabezado";
+    encabezado.innerHTML = `<h1 class="encabezado-titulo">Carrito</h1>`;
+    
+    carritoContenedor.append(encabezado);
+    
+    const button = document.createElement("h1");
+    button.innerText = "X";
+    button.className = "boton-encabezado";
+
+    encabezado.append(boton);
+    
+});
