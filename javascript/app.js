ec340fe58c4595ebdbfde488e95051d00292fbd4
Augusto Carminati <augusto_carminati04@hotmail.com>
2023-07-28 13:08:01 -0300

    se agrego libreria sweetalert y alert compra realizada

--- javascript/app.js
+++ javascript/app.js
@@ -1,106 +1,3 @@
-// let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-// // declaro variables
-// const contenidoTienda = document.getElementById("contenidoTienda");
-// const mostrarCarrito = document.getElementById("mostrarCarrito");
-// const cerrarCarrito = document.getElementById("close");
-// const carritoContenedor = document.getElementById("carritoContenedor");
-// const total = document.getElementById("total");
-
-// /** Creo los productos en el carrito al momento de hacer click en comprar. */
-// const createProductInCart = (products) => {
-//   const productsContainer = document.getElementById("products");
-//   const productsCard = document.createElement("div");
-//   productsCard.className = "products-card";
-
-//   productsCard.innerHTML = `
-//               <div>
-//                   <img
-//                   src="${products.imagen}"
-//                   alt="buzo"
-//                   width="50px"
-//                   height="50px"
-//               />
-//               </div>
-//               <div>
-//                   <h2>${products.nombre}</h2>
-//                   <p>$ ${products.precio}</p>
-//               </div>
-//           `;
-
-//   productsContainer.append(productsCard);
-// };
-
-// /** Renderizo los productos en el carrito. */
-// const renderProducts = (products, contenido) => {
-//   contenido.className = "tarjeta-productos";
-//   contenido.innerHTML = `
-//       <img src="${products.imagen}">
-//       <h3>${products.nombre}</h3>
-//       <p class="precio-producto">$ ${products.precio}</p>
-//       `;
-
-//   contenidoTienda.append(contenido);
-// };
-
-// /** Pusheo los productos en el array del carrito. */
-// const pushProductInCartArray = (products) => {
-//   carrito.push({
-//     imagen: products.imagen,
-//     nombre: products.nombre,
-//     precio: products.precio,
-//   });
-// };
-
-// /** Calculo el total del carrito. */
-// const calculateTotal = () => {
-//   const productsTotal = carrito.reduce((acc, el) => acc + el.precio, 0);
-//   total.innerHTML = "";
-//   total.append(productsTotal);
-// };
-
-// /** Recorro los productos, creo el boton comprar y guardo los productos en el carrito.  */
-// productos.forEach((products) => {
-//   let contenido = document.createElement("div");
-
-//   renderProducts(products, contenido);
-
-//   let comprar = document.createElement("button");
-//   comprar.innerText = "comprar";
-//   comprar.className = "boton-comprar";
-
-//   contenido.append(comprar);
-
-//   comprar.addEventListener("click", () => {
-//     pushProductInCartArray(products);
-//     createProductInCart(products);
-//     calculateTotal();
-//     guardadoLocal();
-//   });
-// });
-
-// /** Muestro los productos del localStorage en el carrito y hago el calculo del total. */
-// if (carrito.length) {
-//   carrito.forEach((product) => {
-//     createProductInCart(product);
-//     calculateTotal();
-//   });
-// }
-
-// mostrarCarrito.addEventListener("click", () => {
-//   document.getElementById("carritoContenedor").style.display = "block";
-// });
-
-// /** Cierro modal */
-// cerrarCarrito.addEventListener("click", () => {
-//   document.getElementById("carritoContenedor").style.display = "none";
-// });
-
-// //aplico localStorage
-// const guardadoLocal = () => {
-//   localStorage.setItem("carrito", JSON.stringify(carrito));
-// };
-
 const consultarProductos = async () => {
   const response = await fetch("../data/productos.json");
   const productos = await response.json();
@@ -111,12 +8,6 @@ const productos = consultarProductos();
 const productosContainer = document.querySelector(".productos__container");
 const countCarrito = document.querySelector("#countCarrito");
 const btnFinalizar = document.querySelector("#btn-finalizar");
-
-const catVga = document.querySelector("#vga");
-const catFuentes = document.querySelector("#fuentes");
-const catMonitores = document.querySelector("#monitores");
-const catProcesadores = document.querySelector("#procesadores");
-const catMothers = document.querySelector("#mothers");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
@@ -249,12 +140,6 @@ function carritoVacio() {
 }
 
 
-
-catVga.addEventListener("click", () => buscarCategoria("vga"));
-catFuentes.addEventListener("click", () => buscarCategoria("fuentes"));
-catMonitores.addEventListener("click", () => buscarCategoria("monitores"));
-catProcesadores.addEventListener("click", () => buscarCategoria("procesadores"));
-catMothers.addEventListener("click", () => buscarCategoria("mothers"));
 btnFinalizar.addEventListener("click", finalizarCompra);
 
 
